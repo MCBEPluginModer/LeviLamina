3a3f0fc65241d83da5567f41081ca54f46b2ecce
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-01-06 22:22:11 +0300

    Update TestCommandRegistry.cpp

--- src/ll/test/TestCommandRegistry.cpp
+++ src/ll/test/TestCommandRegistry.cpp
@@ -32,7 +32,7 @@ class TestCommand : public Command {
 
 
 public:
-    void execute(CommandOrigin const& ori, CommandOutput& output*) const override {
+    void execute(CommandOrigin const& ori, CommandOutput& output) const override {
          auto id = CustomDimensionManager::getInstance().AddDimension("test",123,GeneratorType::Overworld);
         ((Player*)ori.getEntity())->teleport(Vec3(0,100,0),id);
         switch (operation) {
